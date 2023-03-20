fc33e3557c615b5efeaf91b7ee796869a5c3f770
AMmetro <metro.trend.now@gmail.com>
2023-03-20 16:23:53 +0300

    feat: add test start script

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import Search from 'Pages/Search/Search';
+import AboutUs from 'Pages/AboutUs/AboutUs';
+import NotFound from 'Pages/NotFound/NotFound';
+import Header from 'Pages/Header/Header';
 
 test('renders App page', () => {
   render(<App />);
@@ -9,8 +12,26 @@ test('renders App page', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-test('renders Current page', () => {
+test('renders Search page', () => {
   render(<Search />);
   const linkElement = screen.getByText(/Search page/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders about us page', () => {
+  render(<AboutUs />);
+  const linkElement = screen.getByText(/Information/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders not found page', () => {
+  render(<NotFound />);
+  const linkElement = screen.getByText(/wrong url/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders not found page', () => {
+  render(<Header />);
+  const linkElement = screen.getByText(/Current/i);
   expect(linkElement).toBeInTheDocument();
 });
